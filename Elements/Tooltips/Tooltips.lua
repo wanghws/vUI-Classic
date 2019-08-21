41d285e86a33d192b3680fd2c29b51b3e17c93b2
Hydra <ryanandrews135@gmail.com>
2019-08-21 13:24:28 -0600

    more tooltips styling

--- Elements/Tooltips/Tooltips.lua
+++ Elements/Tooltips/Tooltips.lua
@@ -3,8 +3,24 @@ local vUI, GUI, Language, Media, Settings = select(2, ...):get()
 -- Super minimal, don't judge me. So much to do in this file, but I'm just laying out something basic here
 local Tooltips = vUI:NewModule("Tooltips")
 
+local MyGuild
+
 local select = select
 local find = string.find
+local format = format
+local UnitPlayerControlled = UnitPlayerControlled
+local UnitCanAttack = UnitCanAttack
+local UnitIsPVP = UnitIsPVP
+local UnitPVPName = UnitPVPName
+local UnitReaction = UnitReaction
+local UnitExists = UnitExists
+local UnitClass = UnitClass
+local GetGuildInfo = GetGuildInfo
+local UnitRace = UnitRace
+local UnitName = UnitName
+local UnitIsAFK = UnitIsAFK
+local UnitIsDND = UnitIsDND
+local IsInGuild = IsInGuild
 
 Tooltips.Handled = {
 	GameTooltip,
@@ -58,11 +74,11 @@ local UpdateFonts = function(self)
 		end
 	end
 	
-	local MoneyFrame
-	
 	if self.numMoneyFrames then
+		local MoneyFrame
+		
 		for i = 1, self.numMoneyFrames do
-			MoneyFrame = _G[self:GetName() .. "MoneyFrame".. i]
+			MoneyFrame = _G[self:GetName() .. "MoneyFrame" .. i]
 			
 			if (MoneyFrame and not MoneyFrame.Handled) then
 				for j = 1, MoneyFrame:GetNumChildren() do
@@ -89,14 +105,24 @@ end
 
 local SetStyle = function(self)
 	if self.Styled then
+		self.Backdrop:SetBackdrop(vUI.BackdropAndBorder)
+		self.Backdrop:SetBackdropBorderColor(0, 0, 0)
+		self.Backdrop:SetBackdropColorHex(Settings["ui-window-main-color"])
+		
 		UpdateFonts(self)
 		
 		return
 	end
 	
-	self:SetBackdrop(vUI.BackdropAndBorder)
-	self:SetBackdropBorderColor(0, 0, 0)
-	self:SetBackdropColorHex(Settings["ui-window-main-color"])
+	self:SetBackdrop(nil)
+	
+	self.Backdrop = CreateFrame("Frame", nil, self)
+	self.Backdrop:SetAllPoints(self)
+	self.Backdrop:SetBackdrop(vUI.BackdropAndBorder)
+	self.Backdrop:SetBackdropBorderColor(0, 0, 0)
+	self.Backdrop:SetBackdropColorHex(Settings["ui-window-main-color"])
+	self.Backdrop:SetFrameLevel(self:GetFrameLevel() - 1)
+	self.Backdrop:SetFrameStrata("BACKGROUND")
 	
 	self.OuterBG = CreateFrame("Frame", nil, self)
 	self.OuterBG:SetScaledPoint("TOPLEFT", self, -3, 3)
@@ -108,17 +134,13 @@ local SetStyle = function(self)
 	self.OuterBG:SetBackdropColorHex(Settings["ui-window-bg-color"])
 	
 	UpdateFonts(self)
+	
+	self.SetBackdrop = function() end
+	self.SetBackdropColor = function() end
+	self.SetBackdropBorderColor = function() end
+	
+	self.Styled = true
 end
-
-local UnitPlayerControlled = UnitPlayerControlled
-local UnitCanAttack = UnitCanAttack
-local UnitIsPVP = UnitIsPVP
-local UnitPVPName = UnitPVPName
-local UnitReaction = UnitReaction
-local UnitExists = UnitExists
-local UnitClass = UnitClass
-local UnitRace = UnitRace
-local find = string.find
 
 local GetUnitColor = function(unit)
 	local Color
@@ -158,16 +180,23 @@ local OnTooltipSetUnit = function(self)
 		local Race = UnitRace(UnitID)
 		local Level = UnitLevel(UnitID)
 		local Title = UnitPVPName(UnitID)
+		local Guild, Rank = GetGuildInfo(UnitID)
 		local Color = GetUnitColor(UnitID)
 		
 		if (Class == Name) then
 			Class = ""
 		end
 		
+		if UnitIsAFK(Unit) then
+			self:AppendText(" " .. CHAT_FLAG_AFK)
+		elseif UnitIsDND(Unit) then 
+			self:AppendText(" " .. CHAT_FLAG_DND)
+		end
+		
 		if Realm then
-			GameTooltipTextLeft1:SetText(format("|cFF%s%s - %s|r", Color, Title, Realm))
+			GameTooltipTextLeft1:SetText(format("|cFF%s%s - %s|r", Color, (Title or Name), Realm))
 		else
-			GameTooltipTextLeft1:SetText(format("|cFF%s%s|r", Color, Title))
+			GameTooltipTextLeft1:SetText(format("|cFF%s%s|r", Color, (Title or Name)))
 		end
 		
 		local Line
@@ -175,7 +204,14 @@ local OnTooltipSetUnit = function(self)
 		for i = 1, self:NumLines() do
 			Line = _G[self:GetName() .. "TextLeft" .. i]
 			
-			if Line and find(Line:GetText(), "^" .. LEVEL) then
+			if (Line and Guild and (i == 2)) then
+				if (Guild == MyGuild) then
+					Line:SetText(format("|cFF5DADE2%s|r", Guild))
+				else
+					Line:SetText(format("|cFF66BB6A%s|r", Guild))
+				end
+				
+			elseif (Line and find(Line:GetText(), "^" .. LEVEL)) then
 				local LevelColor = vUI:UnitDifficultyColor(UnitID)
 				
 				if Race then
@@ -183,22 +219,41 @@ local OnTooltipSetUnit = function(self)
 				else
 					Line:SetText(format("%s %s%s|r %s", LEVEL, LevelColor, Level, Class))
 				end
-				
-				break
+			elseif (Line and find(Line:GetText(), PVP)) then
+				Line:SetText(format("|cFFEE4D4D%s|r", PVP))
 			end
 		end
 		
 		if (UnitID ~= "player" and UnitExists(UnitID .. "target")) then
 			local TargetColor = GetUnitColor(UnitID .. "target")
 			
-			self:AddLine(Language["|cFFFFFFFFTargeting: |cFF"] .. TargetColor .. UnitName(UnitID .. "target") .. "|r")
+			self:AddLine(Language["Targeting: |cFF"] .. TargetColor .. UnitName(UnitID .. "target") .. "|r", 1, 1, 1)
 		end
 		
-		GameTooltipStatusBar:SetStatusBarColorHex(Color)
+		GameTooltipStatusBar:OldSetStatusBarColor(vUI:HexToRGB(Color))
+		GameTooltipStatusBar.BG:SetVertexColorHex(Color)
 		
 		if self.OuterBG then
 			self.OuterBG:SetScaledPoint("TOPLEFT", self, -3, 3)
 			self.OuterBG:SetScaledPoint("BOTTOMRIGHT", self, 3, -15)
+		end
+	end
+end
+
+local SetTooltipDefaultAnchor = function(self) -- Not actually moving them yet, not sure where to place it.
+	local Unit, UnitID = self:GetUnit()
+	
+	if UnitID then
+		--self:SetAnchorType("ANCHOR_BOTTOMRIGHT", -10, 15)
+			
+		if self.OuterBG then
+			self.OuterBG:SetScaledPoint("BOTTOMRIGHT", self, 3, -15)
+		end
+	else
+		--self:SetAnchorType("ANCHOR_BOTTOMRIGHT", -10, 3)
+		
+		if self.OuterBG then
+			self.OuterBG:SetScaledPoint("BOTTOMRIGHT", self, 3, -3)
 		end
 	end
 end
@@ -209,6 +264,8 @@ function Tooltips:AddHooks()
 	end
 	
 	GameTooltip:HookScript("OnTooltipSetUnit", OnTooltipSetUnit)
+	
+	hooksecurefunc("GameTooltip_SetDefaultAnchor", SetTooltipDefaultAnchor)
 end
 
 function Tooltips:StyleHealth()
@@ -220,6 +277,12 @@ function Tooltips:StyleHealth()
 	HealthBar:SetScaledPoint("TOPRIGHT", HealthBar:GetParent(), "BOTTOMRIGHT", -1, -3)
 	HealthBar:SetStatusBarTexture(Media:GetTexture(Settings["ui-widget-texture"]))
 	
+	HealthBar.BG = HealthBar:CreateTexture(nil, "ARTWORK")
+	HealthBar.BG:SetScaledPoint("TOPLEFT", HealthBar, 0, 0)
+	HealthBar.BG:SetScaledPoint("BOTTOMRIGHT", HealthBar, 0, 0)
+	HealthBar.BG:SetTexture(Media:GetTexture(Settings["ui-widget-texture"]))
+	HealthBar.BG:SetAlpha(0.2)
+	
 	HealthBar.Backdrop = CreateFrame("Frame", nil, HealthBar)
 	HealthBar.Backdrop:SetScaledPoint("TOPLEFT", HealthBar, -1, 1)
 	HealthBar.Backdrop:SetScaledPoint("BOTTOMRIGHT", HealthBar, 1, -1)
@@ -227,6 +290,9 @@ function Tooltips:StyleHealth()
 	HealthBar.Backdrop:SetBackdropColor(0, 0, 0)
 	HealthBar.Backdrop:SetBackdropBorderColor(0, 0, 0)
 	HealthBar.Backdrop:SetFrameLevel(HealthBar:GetFrameLevel() - 1)
+	
+	HealthBar.OldSetStatusBarColor = HealthBar.SetStatusBarColor
+	HealthBar.SetStatusBarColor = function() end
 end
 
 function Tooltips:Load()
@@ -236,6 +302,10 @@ function Tooltips:Load()
 	
 	self:AddHooks()
 	self:StyleHealth()
+	
+	if IsInGuild() then
+		MyGuild = GetGuildInfo("player")
+	end
 end
 
 GUI:AddOptions(function(self)
