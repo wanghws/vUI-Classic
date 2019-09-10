0b02354135108b33ea923b5a35e826c13fec34e9
Hydra <ryanandrews135@gmail.com>
2019-09-09 23:43:01 -0600

    a bag search until I can write bags

--- Elements/Test/Test.lua
+++ Elements/Test/Test.lua
@@ -502,3 +502,34 @@ function Taxi:Load()
 	
 	self.Frame = TaxiFrame
 end
+
+local BagSearch = vUI:NewModule("Bag Search")
+
+local SearchOnTextChanged = function(self)
+	local Text = self:GetText()
+	
+	if Text then
+		SetItemSearch(Text)
+	end
+end
+
+local SearchOnEnterPressed = function(self)
+	self:ClearFocus()
+	self:SetText("")
+end
+
+local SearchOnEditFocusLost = function(self)
+	SetItemSearch("")
+end
+
+function BagSearch:Load()
+	local Search = CreateFrame("EditBox", nil, ContainerFrame1, "InputBoxTemplate")
+	Search:SetScaledPoint("TOPRIGHT", ContainerFrame1, -10, -24)
+	Search:SetScaledSize(120, 30)
+	Search:SetFrameLevel(ContainerFrame1:GetFrameLevel() + 10)
+	Search:SetAutoFocus(false)
+	Search:SetScript("OnTextChanged", SearchOnTextChanged)
+	Search:SetScript("OnEnterPressed", SearchOnEnterPressed)
+	Search:SetScript("OnEscapePressed", SearchOnEnterPressed)
+	Search:SetScript("OnEditFocusLost", SearchOnEditFocusLost)
+end
