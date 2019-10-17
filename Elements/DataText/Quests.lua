99dceedb7a92d72af511fe225df9a99418cda6e8
Hydra <ryanandrews135@gmail.com>
2019-10-16 19:29:53 -0600

    Fill in Quest data text

--- Elements/DataText/Quests.lua
+++ Elements/DataText/Quests.lua
@@ -2,24 +2,30 @@ local vUI, GUI, Language, Media, Settings = select(2, ...):get()
 
 local DT = vUI:GetModule("DataText")
 
+local select = select
+local GetMaxNumQuestsCanAccept = C_QuestLog.GetMaxNumQuestsCanAccept
+local GetNumQuestLogEntries = GetNumQuestLogEntries
 local Label = Language["Quests"]
 
-local Update = function(self, event)
-	--self.Text:SetFormattedText("%s: %s")
+local Update = function(self)
+	local NumQuests = select(2, GetNumQuestLogEntries())
+	local MaxQuests = GetMaxNumQuestsCanAccept()
+	
+	self.Text:SetFormattedText("%s: %s/%s", Label, NumQuests, MaxQuests)
 end
 
 local OnEnable = function(self)
-	self:RegisterEvent("UNIT_STATS")
+	self:RegisterEvent("QUEST_LOG_UPDATE")
 	self:SetScript("OnEvent", Update)
 	
-	self:Update(nil, "player")
+	self:Update()
 end
 
 local OnDisable = function(self)
-	self:UnregisterEvent("UNIT_STATS")
+	self:UnregisterEvent("QUEST_LOG_UPDATE")
 	self:SetScript("OnEvent", nil)
 	
 	self.Text:SetText("")
 end
 
---DT:SetType("Quests", OnEnable, OnDisable, Update)
+DT:SetType("Quests", OnEnable, OnDisable, Update)
