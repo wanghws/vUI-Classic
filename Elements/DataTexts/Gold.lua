b0a41ee0c225becd9aa0945bd923b99680d0c5a4
Hydra <ryanandrews135@gmail.com>
2019-11-12 15:22:16 -0700

    gold data text tooltip

--- Elements/DataTexts/Gold.lua
+++ Elements/DataTexts/Gold.lua
@@ -1,6 +1,7 @@
 local vUI, GUI, Language, Media, Settings = select(2, ...):get()
 
 local DT = vUI:GetModule("DataText")
+local Gold = vUI:GetModule("Gold")
 
 local GetMoney = GetMoney
 local GetCoinTextureString = GetCoinTextureString
@@ -8,9 +9,36 @@ local GetCoinTextureString = GetCoinTextureString
 local OnEnter = function(self)
 	GameTooltip_SetDefaultAnchor(GameTooltip, self)
 	
-	local Total, Profit = vUI:GetTrashValue()
+	local TrashValue = select(2, vUI:GetTrashValue())
+	local ServerInfo, ServerTotalGold = Gold:GetServerInfo()
+	local Gain, Loss = Gold:GetSessionStats()
 	
-	GameTooltip:AddDoubleLine(Language["Trash item vendor value:"], GetCoinTextureString(Profit), 1, 1, 1, 1, 1, 1)
+	GameTooltip:AddLine(format("%s - %s", vUI.UserRealm, vUI.UserFaction))
+	GameTooltip:AddLine(" ")
+	
+	if (#ServerInfo > 1) then
+		GameTooltip:AddDoubleLine(Language["Total"], GetCoinTextureString(ServerTotalGold))
+	end
+	
+	for i = 1, #ServerInfo do
+		GameTooltip:AddDoubleLine(ServerInfo[i][1], GetCoinTextureString(ServerInfo[i][2]))
+	end
+	
+	if ((Gain > 0) or (Loss > 0)) then
+		GameTooltip:AddLine(" ")
+		GameTooltip:AddLine(Language["Session"])
+		
+		if (Gain > Loss) then
+			GameTooltip:AddDoubleLine(Language["Profit"], GetCoinTextureString(Gain))
+		else
+			GameTooltip:AddDoubleLine(Language["Loss"], GetCoinTextureString(Loss))
+		end
+	end
+	
+	if (TrashValue > 0) then
+		GameTooltip:AddLine(" ")
+		GameTooltip:AddDoubleLine(Language["Trash item vendor value:"], GetCoinTextureString(TrashValue))
+	end
 	
 	GameTooltip:Show()
 end
--- Elements/Gold/Gold.lua
+++ Elements/Gold/Gold.lua
@@ -3,10 +3,76 @@ local vUI, GUI, Language, Media, Settings = select(2, ...):get()
 local Gold = vUI:NewModule("Gold")
 
 local GetMoney = GetMoney
-local GetCoinText = GetCoinText -- amount, delim
+local tinsert = table.insert
+local tremove = table.remove
+
+Gold.SessionGain = 0
+Gold.SessionLoss = 0
+Gold.Sorted = {}
+Gold.TablePool = {}
+
+function Gold:GetTable()
+	local Table
+	
+	if self.TablePool[1] then
+		Table = tremove(self.TablePool, 1)
+	else
+		Table = {}
+	end
+	
+	return Table
+end
+
+function Gold:GetColoredName(name)
+	local Color = vUI.ClassColors[vUI.UserClass]
+	local Hex = vUI:RGBToHex(Color[1], Color[2], Color[3])
+	local Name = format("|cFF%s%s|r", Hex, vUI.UserName)
+	
+	return Name
+end
+
+function Gold:GetSessionStats()
+	return self.SessionGain, self.SessionLoss, GetMoney()
+end
+
+function Gold:GetServerInfo()
+	if self.Sorted[1] then
+		for i = 1, #self.Sorted do
+			tinsert(self.TablePool, tremove(self.Sorted, 1))
+		end
+	end
+	
+	local Table
+	local Total = 0
+	
+	for Name, Value in pairs(vUIGold[vUI.UserRealm]) do
+		Table = self:GetTable()
+		
+		Table[1] = Name
+		Table[2] = Value
+		
+		Total = Total + Value
+		
+		tinsert(self.Sorted, Table)
+	end
+	
+	table.sort(self.Sorted, function(a, b)
+		return a[2] > b[2]
+	end)
+	
+	return self.Sorted, Total
+end
 
 function Gold:PLAYER_MONEY()
+	local CurrentValue = GetMoney()
+	local LastValue = vUIGold[vUI.UserRealm][self.CurrentUser]
+	local Diff = CurrentValue - LastValue
 	
+	if (CurrentValue > LastValue) then
+		self.SessionGain = self.SessionGain + Diff
+	else
+		self.SessionLoss = self.SessionLoss - Diff
+	end
 end
 
 function Gold:OnEvent(event)
@@ -15,5 +81,19 @@ function Gold:OnEvent(event)
 	end
 end
 
-Gold:RegisterEvent("LOOT_READY")
-Gold:SetScript("OnEvent", Gold.OnEvent)
+function Gold:Load()
+	if (not vUIGold) then
+		vUIGold = {}
+	end
+	
+	if (not vUIGold[vUI.UserRealm]) then
+		vUIGold[vUI.UserRealm] = {}
+	end
+	
+	self.CurrentUser = self:GetColoredName(vUI.UserName)
+	
+	vUIGold[vUI.UserRealm][self.CurrentUser] = GetMoney()
+	
+	self:RegisterEvent("PLAYER_MONEY")
+	self:SetScript("OnEvent", self.OnEvent)
+end
