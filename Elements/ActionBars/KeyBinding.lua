704b4942408ed732568f5070ac53dbdc0047be1a
Hydra <ryanandrews135@gmail.com>
2019-11-22 16:35:33 -0700

    Update key binding and LibAnim

--- Elements/ActionBars/KeyBinding.lua
+++ Elements/ActionBars/KeyBinding.lua
@@ -6,13 +6,13 @@ local GetMouseFocus = GetMouseFocus
 local match = string.match
 
 KeyBinding.ValidBindings = {
-	["ACTIONBUTTON1"] = true, ["ACTIONBUTTON2"] = true, ["ACTIONBUTTON3"] = true, ["ACTIONBUTTON4"] = true, ["ACTIONBUTTON5"] = true, ["ACTIONBUTTON6"] = true, ["ACTIONBUTTON7"] = true, ["ACTIONBUTTON8"] = true, ["ACTIONBUTTON9"] = true, ["ACTIONBUTTON10"] = true, ["ACTIONBUTTON11"] = true, ["ACTIONBUTTON12"] = true,
-	["BONUSACTIONBUTTON1"] = true, ["BONUSACTIONBUTTON2"] = true, ["BONUSACTIONBUTTON3"] = true, ["BONUSACTIONBUTTON4"] = true, ["BONUSACTIONBUTTON5"] = true, ["BONUSACTIONBUTTON6"] = true, ["BONUSACTIONBUTTON7"] = true, ["BONUSACTIONBUTTON8"] = true, ["BONUSACTIONBUTTON9"] = true, ["BONUSACTIONBUTTON10"] = true,
-	["MULTIACTIONBAR1BUTTON1"] = true, ["MULTIACTIONBAR1BUTTON2"] = true, ["MULTIACTIONBAR1BUTTON3"] = true, ["MULTIACTIONBAR1BUTTON4"] = true, ["MULTIACTIONBAR1BUTTON5"] = true, ["MULTIACTIONBAR1BUTTON6"] = true, ["MULTIACTIONBAR1BUTTON7"] = true, ["MULTIACTIONBAR1BUTTON8"] = true, ["MULTIACTIONBAR1BUTTON9"] = true, ["MULTIACTIONBAR1BUTTON10"] = true, ["MULTIACTIONBAR1BUTTON11"] = true, ["MULTIACTIONBAR1BUTTON12"] = true,
-	["MULTIACTIONBAR2BUTTON1"] = true, ["MULTIACTIONBAR2BUTTON2"] = true, ["MULTIACTIONBAR2BUTTON3"] = true, ["MULTIACTIONBAR2BUTTON4"] = true, ["MULTIACTIONBAR2BUTTON5"] = true, ["MULTIACTIONBAR2BUTTON6"] = true, ["MULTIACTIONBAR2BUTTON7"] = true, ["MULTIACTIONBAR2BUTTON8"] = true, ["MULTIACTIONBAR2BUTTON9"] = true, ["MULTIACTIONBAR2BUTTON10"] = true, ["MULTIACTIONBAR2BUTTON11"] = true, ["MULTIACTIONBAR2BUTTON12"] = true,
-	["MULTIACTIONBAR3BUTTON1"] = true, ["MULTIACTIONBAR3BUTTON2"] = true, ["MULTIACTIONBAR3BUTTON3"] = true, ["MULTIACTIONBAR3BUTTON4"] = true, ["MULTIACTIONBAR3BUTTON5"] = true, ["MULTIACTIONBAR3BUTTON6"] = true, ["MULTIACTIONBAR3BUTTON7"] = true, ["MULTIACTIONBAR3BUTTON8"] = true, ["MULTIACTIONBAR3BUTTON9"] = true, ["MULTIACTIONBAR3BUTTON10"] = true, ["MULTIACTIONBAR3BUTTON11"] = true,
-	["MULTIACTIONBAR3BUTTON12"] = true, ["MULTIACTIONBAR4BUTTON1"] = true, ["MULTIACTIONBAR4BUTTON2"] = true, ["MULTIACTIONBAR4BUTTON3"] = true, ["MULTIACTIONBAR4BUTTON4"] = true, ["MULTIACTIONBAR4BUTTON5"] = true, ["MULTIACTIONBAR4BUTTON6"] = true, ["MULTIACTIONBAR4BUTTON7"] = true, ["MULTIACTIONBAR4BUTTON8"] = true, ["MULTIACTIONBAR4BUTTON9"] = true, ["MULTIACTIONBAR4BUTTON10"] = true, ["MULTIACTIONBAR4BUTTON11"] = true, ["MULTIACTIONBAR4BUTTON12"] = true,
-	["SHAPESHIFTBUTTON1"] = true, ["SHAPESHIFTBUTTON2"] = true, ["SHAPESHIFTBUTTON3"] = true, ["SHAPESHIFTBUTTON4"] = true, ["SHAPESHIFTBUTTON5"] = true, ["SHAPESHIFTBUTTON6"] = true, ["SHAPESHIFTBUTTON7"] = true, ["SHAPESHIFTBUTTON8"] = true, ["SHAPESHIFTBUTTON9"] = true, ["SHAPESHIFTBUTTON10"] = true,
+	["ACTIONBUTTON"] = true,
+	["BONUSACTIONBUTTON"] = true,
+	["MULTIACTIONBAR1BUTTON"] = true,
+	["MULTIACTIONBAR2BUTTON"] = true,
+	["MULTIACTIONBAR3BUTTON"] = true,
+	["MULTIACTIONBAR4BUTTON"] = true,
+	["SHAPESHIFTBUTTON"] = true,
 }
 
 KeyBinding.Translate = {
@@ -70,13 +70,10 @@ function KeyBinding:OnKeyDown(key)
 	if (MouseFocus and MouseFocus.GetName) then
 		local Name = MouseFocus:GetName()
 		local ButtonName = match(Name, "%D+")
-		local ActionNum = match(Name, "(%d+)$")
 		
 		if self.Translate[ButtonName] then
-			local BindingName = self.Translate[ButtonName] .. ActionNum
-			
-			if self.ValidBindings[BindingName] then
-				self.TargetBindingName = BindingName
+			if self.ValidBindings[self.Translate[ButtonName]] then
+				self.TargetBindingName = self.Translate[ButtonName] .. match(Name, "(%d+)$")
 				self.TargetName = Name
 			end
 		end
@@ -173,9 +170,9 @@ end
 	local Left, Right = self:GetWindow(Language["Action Bars"])
 	
 	Right:CreateHeader(Language["Key Binding"])
-	Right:CreateButton(Language["Toggle"], Language["Key Bind Mode"], "While toggled, you can hover over action buttons|nand press a key combination to rebind that action", ToggleBindingMode)
-	Right:CreateButton(Language["Save"], Language["Save Changes"], "Save key binding changes", SaveChanges)
-	Right:CreateButton(Language["Discard"], Language["Discard Changes"], "Discard key binding changes", DiscardChanges)
+	Right:CreateButton(Language["Toggle"], Language["Key Bind Mode"], Language["While toggled, you can hover over action buttons|nand press a key combination to rebind that action"], ToggleBindingMode)
+	Right:CreateButton(Language["Save"], Language["Save Changes"], Language["Save key binding changes"], SaveChanges)
+	Right:CreateButton(Language["Discard"], Language["Discard Changes"], Language["Discard key binding changes"], DiscardChanges)
 	
 	self:GetWidgetByWindow(Language["Action Bars"], "save"):Disable()
 	self:GetWidgetByWindow(Language["Action Bars"], "discard"):Disable()
