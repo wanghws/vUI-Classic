d419836ab451447f2e2fc10e90cbb79cf16fa374
Hydra <ryanandrews135@gmail.com>
2019-11-28 14:47:51 -0700

    :eyes: here come unit frame options

--- vUI/Elements/Defaults/Defaults.lua
+++ vUI/Elements/Defaults/Defaults.lua
@@ -277,19 +277,31 @@ Defaults["announcements-channel"] = "GROUP"
 
 -- Unitframes -- Add option to color health backdrops by reaction/class color, or use health color, or set a color
 Defaults["unitframes-enable"] = true
-Defaults["unitframes-enable-raid"] = true
-Defaults["unitframes-enable-party"] = true
-Defaults["unitframes-enable-party-pets"] = true
-Defaults["unitframes-player-show-name"] = false
-Defaults["unitframes-player-cc-health"] = false
-Defaults["unitframes-target-cc-health"] = false
 Defaults["unitframes-class-color"] = true -- temporary
 Defaults["unitframes-only-player-debuffs"] = false
 Defaults["unitframes-show-player-buffs"] = true
-Defaults["unitframes-show-mana-timer"] = false
-Defaults["unitframes-show-energy-timer"] = true
 
+Defaults["unitframes-player-width"] = 238
+Defaults["unitframes-player-health-height"] = 28
+Defaults["unitframes-player-power-height"] = 15
+
+Defaults["unitframes-target-width"] = 238
+Defaults["unitframes-target-health-height"] = 28
+Defaults["unitframes-target-power-height"] = 15
+
+Defaults["unitframes-targettarget-width"] = 110
+Defaults["unitframes-targettarget-health-height"] = 26
+--Defaults["unitframes-targettarget-power-height"] = 15
+
+Defaults["unitframes-pet-width"] = 110
+Defaults["unitframes-pet-health-height"] = 26
+--Defaults["unitframes-pet-power-height"] = 15
+
+Defaults["party-enable"] = true
+Defaults["party-pets-enable"] = true
 Defaults["party-show-debuffs"] = true
+
+Defaults["raid-enable"] = true
 
 -- Name Plates
 Defaults["nameplates-enable"] = true
